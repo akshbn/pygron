179b2be419d4d049ac1dee509477ed966169b57e
Akshay B N <akshbn@users.noreply.github.com>
2016-08-23 07:22:53 +0530

    Bumped up version to 0.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
 name = 'pygron',
-version = '0.2.2',
+version = '0.2.9',
 license = 'MIT',
 author = 'Akshay B N',
 description = 'Helps JSON become greppable',
